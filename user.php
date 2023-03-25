91564d5355a82cb438bb310d3aeb0cd991f26514
999BZ <blendizeqiri@hotmail.com>
2023-03-25 15:02:48 +0100

    Update user.php

--- user.php
+++ user.php
@@ -6,100 +6,79 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.css">
     <title>Document</title>
+    <style>
+      
+    </style>
 </head>
 <body>
-    <!-- Section: Design Block -->
-<section class="">
-  <!-- Jumbotron -->
-  <div class="px-4 py-5 px-md-5 text-center text-lg-start" style="background-color: hsl(0, 0%, 96%)">
-    <div class="container">
-      <div class="row gx-lg-5 align-items-center">
-        <div class="col-lg-6 mb-5 mb-lg-0">
-          <h1 class="my-5 display-3 fw-bold ls-tight">
-            The best offer <br />
-            <span class="text-primary">for your business</span>
-          </h1>
-          <p style="color: hsl(217, 10%, 50.8%)">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Eveniet, itaque accusantium odio, soluta, corrupti aliquam
-            quibusdam tempora at cupiditate quis eum maiores libero
-            veritatis? Dicta facilis sint aliquid ipsum atque?
-          </p>
-        </div>
+<div class="container">
+		<div class="row justify-content-center mt-5">
+			<div class="col-md-6">
 
-        <div class="col-lg-6 mb-5 mb-lg-0">
-          <div class="card">
-            <div class="card-body py-5 px-md-5">
-              <form>
-                <!-- 2 column grid layout with text inputs for the first and last names -->
-                <div class="row">
-                  <div class="col-md-6 mb-4">
-                    <div class="form-outline">
-                      <input type="text" id="form3Example1" class="form-control" />
-                      <label class="form-label" for="form3Example1">First name</label>
-                    </div>
-                  </div>
-                  <div class="col-md-6 mb-4">
-                    <div class="form-outline">
-                      <input type="text" id="form3Example2" class="form-control" />
-                      <label class="form-label" for="form3Example2">Last name</label>
-                    </div>
-                  </div>
-                </div>
+				<!-- Login Form -->
+				<form id="login-form">
+					<h2>Login</h2>
+					<div class="form-group">
+						<label for="login-username">Username:</label>
+						<input type="text" class="form-control" id="login-username" name="login-username">
+					</div>
 
-                <!-- Email input -->
-                <div class="form-outline mb-4">
-                  <input type="email" id="form3Example3" class="form-control" />
-                  <label class="form-label" for="form3Example3">Email address</label>
-                </div>
+					<div class="form-group">
+						<label for="login-password">Password:</label>
+						<input type="password" class="form-control" id="login-password" name="login-password">
+					</div>
 
-                <!-- Password input -->
-                <div class="form-outline mb-4">
-                  <input type="password" id="form3Example4" class="form-control" />
-                  <label class="form-label" for="form3Example4">Password</label>
-                </div>
+					<button type="submit" class="btn btn-primary">Login</button>
 
-                <!-- Checkbox -->
-                <div class="form-check d-flex justify-content-center mb-4">
-                  <input class="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
-                  <label class="form-check-label" for="form2Example33">
-                    Subscribe to our newsletter
-                  </label>
-                </div>
+					<p>Don't have an account? <a href="#" id="register-link">Register here</a>.</p>
+				</form>
 
-                <!-- Submit button -->
-                <button type="submit" class="btn btn-primary btn-block mb-4">
-                  Sign up
-                </button>
+				<!-- Register Form -->
+				<form id="register-form" style="display: none;">
+					<h2>Register</h2>
+					<div class="form-group">
+						<label for="register-username">Username:</label>
+						<input type="text" class="form-control" id="register-username" name="register-username">
+					</div>
 
-                <!-- Register buttons -->
-                <div class="text-center">
-                  <p>or sign up with:</p>
-                  <button type="button" class="btn btn-link btn-floating mx-1">
-                    <i class="fab fa-facebook-f"></i>
-                  </button>
+					<div class="form-group">
+						<label for="register-password">Password:</label>
+						<input type="password" class="form-control" id="register-password" name="register-password">
+					</div>
 
-                  <button type="button" class="btn btn-link btn-floating mx-1">
-                    <i class="fab fa-google"></i>
-                  </button>
+					<div class="form-group">
+						<label for="confirm-password">Confirm Password:</label>
+						<input type="password" class="form-control" id="confirm-password" name="confirm-password">
+					</div>
 
-                  <button type="button" class="btn btn-link btn-floating mx-1">
-                    <i class="fab fa-twitter"></i>
-                  </button>
+					<button type="submit" class="btn btn-primary">Register</button>
 
-                  <button type="button" class="btn btn-link btn-floating mx-1">
-                    <i class="fab fa-github"></i>
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- Jumbotron -->
-</section>
-<!-- Section: Design Block -->
+					<p>Already have an account? <a href="#" id="login-link">Login here</a>.</p>
+				</form>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+	<!-- Script to switch between login and register forms -->
+	<script>
+		$(document).ready(function() {
+			$('#register-link').click(function() {
+				$('#login-form').hide();
+				$('#register-form').show();
+			});
+
+			$('#login-link').click(function() {
+				$('#register-form').hide();
+				$('#login-form').show();
+			});
+		});
+	</script>
+
 </body>
 </html>
