bf489494f26caad50a66e727fd2fd23ebda75d3a
999BZ <blendizeqiri@hotmail.com>
2023-03-26 17:27:44 +0200

    Update searchM.js

--- searchM.js
+++ searchM.js
@@ -4,63 +4,6 @@ import {getDatabase, ref, get, set, child, update, remove}
     const db = getDatabase();
     
     
-    // let results = [];
-    // function SelectData(){
-    //     let movie = document.getElementById("movieSearch").value.toUpperCase();
-    //     console.log(movie);
-    //     var divChildren = document.getElementById("searchResult").children;
-    //     // for(let i = 0 ; i < divChildren.length;i++){
-    //     //     if(!divChildren[i].alt.includes(movie)){
-    //     //         document.getElementById("searchResult").removeChild(divChildren[i]);
-    //     //         console.log("Child " + i + " was removed!");
-    //     //     }
-    //     // }
-        
-    //     var letters = /^[A-Za-z0-9]+$/;
-    //     if(letters.test(movie)){
-    //         const dbref = ref(db);
-    //         for(let i = 1;i<8;i++){
-    //             let temp = false;
-    //             get(child(dbref, "movies/" + i)).then((snapshot) => {
-    //                 if(snapshot.val().Title.toUpperCase().includes(movie)){
-    //                     if(snapshot.exists()){
-    //                         var imgTest1 = document.createElement('img');
-    //                         imgTest1.width=200;
-    //                         imgTest1.src=snapshot.val().Cover;
-    //                         imgTest1.alt=snapshot.val().Title;
-    //                         imgTest1.className="sResults";
-    //                     }else{
-    //                         console.log("Doesn\'t exist!");
-    //                     }
-    //                     for(let j = 0; j<divChildren.length;j++){
-    //                         if(imgTest1.alt == divChildren[j].alt){
-    //                             temp=true;
-    //                             break;
-    //                         }
-    //                     }
-    //                     if(temp==false){
-    //                         document.getElementById("searchResult").appendChild(imgTest1);
-    //                         results.push(imgTest1);
-    //                     }
-                        
-    //                 }
-    //             })
-    //         }
-    //     }
-    //     console.log(results);
-    //     results.forEach((r,index) => {
-    //         const contains = (r.alt.toUpperCase().indexOf(movie)>-1);
-    //         console.log(r.alt+" contains: " + contains);
-    //         for(let i = 0; i<divChildren.length;i++){
-    //             if(r.alt == divChildren[i].alt && contains == false){
-    //                 document.getElementById("searchResult").removeChild(divChildren[i]);
-    //                 results.splice(index,1);
-    //             }
-    //         }
-    //     });
-    // }
-let results = [];
-
 function SelectData() {
     let movie = document.getElementById("movieSearch").value.toUpperCase();
     const dbref = ref(db);
@@ -70,8 +13,8 @@ function SelectData() {
     if(movie !== "" && movie.length > 1){
         for (let i = 1; i < 20; i++) {
             get(child(dbref, "movies/" + i)).then((snapshot) => {
-                if (snapshot.val().Title.toUpperCase().indexOf(movie) > -1) {
-                    if (snapshot.exists()) {
+                if (snapshot.exists()) {
+                    if (snapshot.val().Title.toUpperCase().indexOf(movie) > -1) {
                         var imgTest1 = document.createElement('img');
                         imgTest1.width = 200;
                         imgTest1.src = snapshot.val().Cover;
@@ -88,9 +31,9 @@ function SelectData() {
                                 document.getElementById("searchResult").appendChild(imgTest1);
                             }
                         }
-                    }else {
-                        console.log("Doesn't exist!");
                     }
+                }else{
+                    console.log("Doesn't exist!");
                 }
 
             })
@@ -121,6 +64,7 @@ function SelectData() {
     }
 
 }
+
 
 
     let sub = document.getElementById("Test12345");
