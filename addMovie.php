7da26296190aae0844efb16b246143c58f4ea2f7
999BZ <blendizeqiri@hotmail.com>
2023-04-03 17:58:48 +0200

    minor changes

--- addMovie.php
+++ addMovie.php
@@ -98,8 +98,20 @@
     </div>
   </aside>
 
+  
   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
-
+    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="true">
+        <div class="container-fluid py-1 px-3">
+            <nav aria-label="breadcrumb">
+            <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
+                <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="movies-tb.php">Movie table</a></li>
+                <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Add Movie</li>
+            </ol>
+            <h6 class="font-weight-bolder mb-0">Add Movie</h6>
+            </nav>
+        </div>
+        </nav>
   <div class="row justify-content-center mt-5" id="movieadd">
         <div class="col-md-6" style="display: flex; justify-content: center;">
           <form id="movie-add" method="post">
