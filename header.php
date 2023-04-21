e405d41b658e2b9775aea4dc1f60c6065471682b
999BZ <blendizeqiri@hotmail.com>
2023-04-21 20:01:34 +0200

    Updated Authentication of Email

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-<?php session_start();?>
+<?php session_start(); ?>
 <style>
     .header {
         position: fixed;
@@ -12,13 +12,15 @@
     .header.hide {
         top: -100px;
     }
-    #searchresult a:hover h6{
+
+    #searchresult a:hover h6 {
         transition: 0.6s;
-        color:#00d9e1;
+        color: #00d9e1;
     }
-    #searchresult a:hover~a h6{
+
+    #searchresult a:hover~a h6 {
         transition: 0.6s;
-        color:#00d9e1;
+        color: #00d9e1;
     }
 </style>
 
@@ -39,29 +41,30 @@
                         <li><a class="tv" href="tv-shows.php">Tv Shows</a></li>
                         <li><a class="imdb" href="imdb.php">Top IMDb</a></li>
                         <li>
-                            <?php 
-                                if (isset($_SESSION['user_logged_in']) && $_SESSION['user_logged_in'] == true) {
-                                    echo '<a href="#">Profile <i class="icofont icofont-simple-down"></i></a>
+                            <?php
+                            if (isset($_SESSION['user_logged_in']) && $_SESSION['user_logged_in'] == true) {
+                                echo '<a href="#">Profile <i class="icofont icofont-simple-down"></i></a>
                                             <ul>
                                                 <li><a href="user.php">Edit</a></li>';
-                                                    if(isset($_SESSION['admin']) && $_SESSION['admin'] == 1){
-                                                        echo '<li><a class="dashboard" href="dashboard/dashboard.php" target="_blank">Dashboard</a></li>';
-                                                    }
-                                            echo'<li><a href="logout.php">Sign out</a></li>';
-                                        
-                                    echo '</ul>';
-                                }else{
-                                    echo '<a href="#">Profile <i class="icofont icofont-simple-down"></i></a>
+                                if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1) {
+                                    echo '<li><a class="dashboard" href="dashboard/dashboard.php" target="_blank">Dashboard</a></li>';
+                                }
+                                echo '<li><a href="logout.php">Sign out</a></li>';
+
+                                echo '</ul>';
+                            } else {
+                                echo '<a href="#">Profile <i class="icofont icofont-simple-down"></i></a>
                                             <ul>
                                                 <li><a href="" class="signup-popup">Log in</a></li>
                                                 <li><a href="" class="login-popup">Sign up</a></li>
                                             </ul>';
-                                }
+                            }
                             ?>
-                        </li> 
+                        </li>
                         <li>
                             <form style="display: flex;" method="post" action="results.php" id="myForm">
-                                <input type="text" name="search" placeholder="Search..." class="form-control" id="live_search" autocomplete="off">
+                                <input type="text" name="search" placeholder="Search..." class="form-control"
+                                    id="live_search" autocomplete="off">
                                 <input type="submit" name="submit" value="Go" id="submit" style="visibility: hidden;">
 
                             </form>
@@ -85,7 +88,7 @@
                     data: {
                         input: input
                     },
-                    success: function(data) {
+                    success: function (data) {
                         $("#searchresult").html(data);
                     }
                 });
@@ -95,23 +98,23 @@
             }
         }
 
-        document.getElementById('live_search').addEventListener('input', function() {
+        document.getElementById('live_search').addEventListener('input', function () {
             liveSearch();
             if (document.getElementById('live_search').value.length >= 2) {
                 document.getElementById('submit').disabled = false;
-                
+
             } else {
                 document.getElementById('submit').disabled = true;
             }
         });
-        
-        function submitForm(){
+
+        function submitForm() {
             var form = document.getElementById('myForm');
 
             form.submit();
         }
-        
-        
+
+
     </script>
     <!-- <script>
         var anchorTag = document.getElementById('myAnchorTag');
@@ -133,7 +136,8 @@
             <h6 style="color: white;">EMAIL ADDRESS</h6>
             <input type="text" id="email-field-login" name="email-field-login" style="color: white;" />
             <h6 style="color: white;">PASSWORD</h6>
-            <input style="color: white;" type="password" id="password-field-login" name="password-field-login" class="field input" required placeholder="Password" />
+            <input style="color: white;" type="password" id="password-field-login" name="password-field-login"
+                class="field input" required placeholder="Password" />
             <div class="login-remember">
                 <input style="color: white;" type="checkbox" />
                 <span style="color: white;">Remember Me</span>
@@ -157,16 +161,20 @@
         <form method="post">
             <input type="hidden" name="signUpForm" value="1">
             <h6 style="color: white;">USERNAME</h6>
-            <input style="color: white;" type="text" id="username-field" name="username-field" class="field input" required />
+            <input style="color: white;" type="text" id="username-field" name="username-field" class="field input"
+                required />
             <h6 style="color: white;">EMAIL ADDRESS</h6>
-            <input style="color: white;" type="email" id="email-field" name="email-field" class="field input" required />
+            <input style="color: white;" type="email" id="email-field" name="email-field" class="field input"
+                required />
             <h6 style="color: white;">PASSWORD</h6>
-            <input style="color: white;" type="password" id="password-field" name="password-field" class="field input" required onkeyup="verifyPassword()" />
+            <input style="color: white;" type="password" id="password-field" name="password-field" class="field input"
+                required onkeyup="verifyPassword()" />
             <p id="all" style="display: none; justify-content: center; color: white;">Password must contain at least one
                 capital
                 letter, one digit and must be at least 8 characters long!</p>
             <h6 style="color: white;">CONFIRM PASSWORD</h6>
-            <input style="color: white;" type="password" id="password-field2" name="password-field2" class="field input" required onkeyup="verifyPassword()" />
+            <input style="color: white;" type="password" id="password-field2" name="password-field2" class="field input"
+                required onkeyup="verifyPassword()" />
             <p id="isItSame" style="display: none; justify-content: center;" style="color: white;">Passwords don't match
             </p>
             <div style="color: white;" class="login-remember">
@@ -186,121 +194,145 @@
     </div>
 </div>
 <?php
-    $db_host = 'localhost';
-    $db_user = 'root';
-    $db_pass = 'root';
-    $db_name = 'moviedb';
-    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name, 3307);
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = 'root';
+$db_name = 'moviedb';
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name, 3307);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['signUpForm'])) {
+    $username = $_POST['username-field'];
+    $email = $_POST['email-field'];
+    $password = $_POST['password-field'];
+
+    $stmt = mysqli_prepare($conn, "SELECT * FROM users WHERE email = ?");
+    mysqli_stmt_bind_param($stmt, 's', $email);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    $stmt1 = mysqli_prepare($conn, "SELECT * FROM users WHERE username = ?");
+    mysqli_stmt_bind_param($stmt1, 's', $username);
+    mysqli_stmt_execute($stmt1);
+    $result1 = mysqli_stmt_get_result($stmt1);
+    
+    if (mysqli_num_rows($result) == 0 && mysqli_num_rows($result1) == 0) {
+        $message = bin2hex(random_bytes(32));
+        $salt = bin2hex(random_bytes(16));
+        $hashed_token = hash('sha256', $message . $salt);
+        require("send-grid/sendgrid-php.php");
+                $SGemail = new \SendGrid\Mail\Mail();
+                $SGemail->setFrom("flixfeastt@gmail.com", "FlixFeast");
+                $SGemail->setTemplateId("d-b86719bb7e9a4cafbd0ef38bd39bb15c");
+                $SGemail->addDynamicTemplateData('username', $username);
+                $SGemail->addDynamicTemplateData('actToken', $hashed_token);
+                $SGemail->addTo($email, "Example User");
+                $sendgrid = new \SendGrid('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');
+                try {
+                    $response = $sendgrid->send($SGemail);
+                } catch (Exception $e) {
+                    echo 'Caught exception: '. $e->getMessage() ."\n";
+                }
+        $tempInfo = fopen("tempReg.txt", "w");
+        fwrite($tempInfo, "Username: " . $username);
+        fwrite($tempInfo, "\nPassword: " . $password);
+        fwrite($tempInfo, "\nEmail: " . $email);
+        fwrite($tempInfo, "\nToken: " . $hashed_token);
+        fclose($tempInfo);
+    }else{
+        echo '<div class="overlay">
+                    <div class="modal">
+                        <p>Username or E-mail already in use</p>
+                    </div>
+                </div>';
+    }
+}
+if(isset($_GET['activation_token'])){
+        $tempInfos = fopen("tempReg.txt", "r");
+        $tempUsername = "";
+        $tempPassword = "";
+        $tempEmail = "";
+        $tempToken = "";
+        while(!feof($tempInfos)) {
+            $line = fgets($tempInfos);
+            if (strpos($line, "Username: ") !== false) {
+                $tempUsername = trim(str_replace("Username: ", "", $line));
+            }
+            if (strpos($line, "Password: ") !== false) {
+                $tempPassword = trim(str_replace("Password: ", "", $line));
+            }
+            if (strpos($line, "Email: ") !== false) {
+                $tempEmail = trim(str_replace("Email: ", "", $line));
+            }
+            if (strpos($line, "Token: ") !== false) {
+                $tempToken = trim(str_replace("Token: ", "", $line));
+            }
+        }
+        if($_GET['activation_token'] == $tempToken){
+            $stmt = $conn->prepare("INSERT INTO users (Username, Email, password) VALUES (?, ?, ?)");
+                    $stmt->bind_param("sss", $tempUsername, $tempEmail, $tempPassword);
+                    $stmt->execute();
+
+                    if (mysqli_stmt_affected_rows($stmt) > 0) {
+                        echo "<script>console.log('Data inserted successfully!')</script>";
+                        $_SESSION['user_logged_in'] = true;
+                        $_SESSION['user'] = false;
+                        unlink("tempReg.txt");
+                        echo '<script>window.location.href = "index.php";</script>';
+                        $tempToken = "";
+                        
+                    } else {
+                        echo "<script>console.log('Error inserting data')</script>";
+        }
+        
+    }
+    }
+?>
+<?php
+if (isset($_POST['logInForm'])) {
+    $email = $_POST['email-field-login'];
+    $password = $_POST['password-field-login'];
 
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['signUpForm'])) {
-        $username = $_POST['username-field'];
-        $email = $_POST['email-field'];
-        $password = $_POST['password-field'];
+    $stmt = mysqli_prepare($conn, "SELECT * FROM users WHERE email = ?");
+    mysqli_stmt_bind_param($stmt, 's', $email);
+    mysqli_stmt_execute($stmt);
 
-        $stmt = mysqli_prepare($conn, "SELECT * FROM users WHERE email = ?");
-        mysqli_stmt_bind_param($stmt, 's', $email);
-        mysqli_stmt_execute($stmt);
-        $result = mysqli_stmt_get_result($stmt);
+    $result = mysqli_stmt_get_result($stmt);
 
-        $stmt1 = mysqli_prepare($conn, "SELECT * FROM users WHERE username = ?");
-        mysqli_stmt_bind_param($stmt1, 's', $username);
-        mysqli_stmt_execute($stmt1);
-        $result1 = mysqli_stmt_get_result($stmt1);
-
-        if(mysqli_num_rows($result) == 0 && mysqli_num_rows($result1) == 0) {
-            $stmt = $conn->prepare("INSERT INTO users (Username, Email, password) VALUES (?, ?, ?)");
-            $stmt->bind_param("sss", $username, $email, $password);
-            $stmt->execute();
-
-            if (mysqli_stmt_affected_rows($stmt) > 0) {
-                echo "<script>console.log('Data inserted successfully!</script>')";
-                $_SESSION['user_logged_in'] = true;
-                $_SESSION['user'] = false;
-                // require 'vendor/autoload.php'; // If you're using Composer (recommended)
-// Comment out the above line if not using Composer
-require("<PATH TO>/send-grid/sendgrid-php.php");
-// If not using Composer, uncomment the above line and
-// download sendgrid-php.zip from the latest release here,
-// replacing <PATH TO> with the path to the sendgrid-php.php file,
-// which is included in the download:
-// https://github.com/sendgrid/sendgrid-php/releases
-
-$email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("test@example.com", "Example User");
-$email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("test@example.com", "Example User");
-$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-$email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-);
-$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
-} catch (Exception $e) {
-    echo 'Caught exception: '. $e->getMessage() ."\n";
+    if (mysqli_num_rows($result) == 1) {
+        $user = mysqli_fetch_assoc($result);
+        if ($password == $user['Password']) {
+            $_SESSION['user_logged_in'] = true;
+            $_SESSION['user'] = $user['ID'];
+            $_SESSION['admin'] = $user['Admin'];
+            echo '<script>window.location.href = "index.php";</script>';
+        } else {
+            echo "<script>alert('Incorrect password');</script>";
+        }
+    } else {
+        echo "<script>alert('User not found');</script>";
+    }
 }
-
-            } else {
-                echo "<script>console.log('Error inserting data')</script>";
-            }
-        } else {
-            echo '<div class="overlay">
-                    <div class="modal">
-                        <p>Username or E-mail already in use</p>
-                    </div>
-                </div>';
-        }
-    }
-?> 
-<?php 
-    if (isset($_POST['logInForm'])) {
-        $email = $_POST['email-field-login'];
-        $password = $_POST['password-field-login'];
-
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-    
-        $stmt = mysqli_prepare($conn, "SELECT * FROM users WHERE email = ?");
-        mysqli_stmt_bind_param($stmt, 's', $email);
-        mysqli_stmt_execute($stmt);
-
-        $result = mysqli_stmt_get_result($stmt);
-    
-        if (mysqli_num_rows($result) == 1) {
-            $user = mysqli_fetch_assoc($result);
-            if ($password == $user['Password']) {
-                $_SESSION['user_logged_in'] = true;
-                $_SESSION['user'] = $user['ID'];
-                $_SESSION['admin'] = $user['Admin'];
-                echo '<script>window.location.href = "index.php";</script>';
-            } else {
-                echo "<script>alert('Incorrect password');</script>";
-            }
-        } else {
-            echo "<script>alert('User not found');</script>";
-        }
-    }
 ?>
 <?php
-    // function isAdmin(){
-    //     $stmt = mysqli_prepare($conn, "SELECT * FROM users WHERE email = ?");
-    //     mysqli_stmt_bind_param($stmt, 's', $_SESSION['user']);
-    //     mysqli_stmt_execute($stmt);
-    //     $result = mysqli_stmt_get_result($stmt);
-    //     $user = mysqli_fetch_assoc($result);
-    //     if ($user['Admin'] == 1){
-    //         return true;
-    //     } 
-    //     return false;
-    // }
+// function isAdmin(){
+//     $stmt = mysqli_prepare($conn, "SELECT * FROM users WHERE email = ?");
+//     mysqli_stmt_bind_param($stmt, 's', $_SESSION['user']);
+//     mysqli_stmt_execute($stmt);
+//     $result = mysqli_stmt_get_result($stmt);
+//     $user = mysqli_fetch_assoc($result);
+//     if ($user['Admin'] == 1){
+//         return true;
+//     } 
+//     return false;
+// }
 ?>
 <script>
     function verifyPassword() {
