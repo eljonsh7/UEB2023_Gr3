f69399fdcd4d408c4d177d391ba945cb248d8763
999BZ <blendizeqiri@hotmail.com>
2023-04-21 23:31:24 +0200

    Updated Security

--- dashboard/addMovie.php
+++ dashboard/addMovie.php
@@ -1,3 +1,10 @@
+<?php 
+    session_start();
+    if(!isset($_SESSION['admin']) || $_SESSION['admin']!= 1){
+        echo '<script>window.location.href = "../index.php";</script>';
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="zxx">
 
